c6072f9482cd99a86b99f4846913e9a4693abf90
FarCodeFR <Timo-44@hotmail.com>
2025-02-12 14:39:53 +0100

    Création du formulaire d'inscription et de registre pour accéder a l'accueil

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,27 @@
+import { NavLink } from "react-router-dom";
+import "../styles/login.css";
+
 function Login() {
-  return <h1>Bienvenue sur Code-Bloc</h1>;
+  return (
+    <section className="container-form-login">
+      <h1>Ravie de vous revoir</h1>
+      <form>
+        <label htmlFor="email">Enter your email:</label>
+        <input
+          type="email"
+          placeholder="codeBloc@live.fr"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="pwd">Password:</label>
+        <input type="password" placeholder="*******" id="pwd" name="pwd" />
+      </form>
+      <section>
+        <NavLink to="/register">S'inscrire</NavLink>
+        <NavLink to="/login">Se connecter</NavLink>
+      </section>
+    </section>
+  );
 }
 
 export default Login;
